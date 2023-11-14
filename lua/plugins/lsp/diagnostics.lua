d369946dd03d52be01e3620ed478b1e1dc14947c
dagregi <120724828+dagregi@users.noreply.github.com>
2023-11-14 15:32:32 +0300

    autocmd refactor

--- lua/plugins/lsp/diagnostics.lua
+++ lua/plugins/lsp/diagnostics.lua
@@ -1,33 +1,13 @@
+local M = {}
+
 local fmt = string.format
+
 local icons = {
 	error = " ",
 	warn = " ",
 	hint = " ",
 	info = " ",
 }
-
-local M = {}
-
-function M.augroup(name, ...)
-	local commands = { ... }
-	assert(name ~= "User", "The name of an augroup CANNOT be User")
-	assert(#commands > 0, fmt("You must specify at least on autocommand for %s", name))
-	local group_id = vim.api.nvim_create_augroup(name, { clear = true })
-	for _, autocmd in ipairs(commands) do
-		local is_callback = type(autocmd.command) == "function"
-		vim.api.nvim_create_autocmd(autocmd.event, {
-			group = group_id,
-			pattern = autocmd.pattern,
-			desc = autocmd.desc,
-			callback = is_callback and autocmd.command or nil,
-			command = not is_callback and autocmd.command or nil,
-			once = autocmd.once,
-			nested = autocmd.nested,
-			buffer = autocmd.buffer,
-		})
-	end
-	return group_id
-end
 
 function M.setup()
 	for name, icon in pairs(icons) do
@@ -66,7 +46,7 @@ function M.setup()
 		},
 	})
 
-	M.augroup("LspDiagnostics", {
+	require("config.utils").augroup("LspDiagnostics", {
 		event = "CursorHold",
 		desc = "LSP: show diagnostics",
 		command = function()
