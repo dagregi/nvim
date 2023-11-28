1e45b8b6bc58efefa4e431ee1d75ad2ee9ee7a9f
dagregi <120724828+dagregi@users.noreply.github.com>
2023-11-28 07:40:55 +0300

    Playing around with harpoon again

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,31 @@
+return {
+	"ThePrimeagen/harpoon",
+	keys = {
+		{
+			"<C-g>",
+			function()
+				require("harpoon.ui").toggle_quick_menu()
+			end,
+			desc = "Harpoon UI",
+		},
+	},
+	config = function()
+		local ui = require("harpoon.ui")
+		local mark = require("harpoon.mark")
+		local remap = require("config.utils").remap
+
+		remap("n", "<leader>ha", function()
+			mark.add_file()
+		end, "Add file")
+		remap("n", "<leader>hc", function()
+			mark.clear_all()
+		end, "Clear marks")
+
+		remap("n", "<C-n>", function()
+			ui.nav_next()
+		end, "Next mark")
+		remap("n", "<C-p>", function()
+			ui.nav_prev()
+		end, "Prev mark")
+	end,
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,20 +2,10 @@ return {
 	"stevearc/oil.nvim",
 	keys = {
 		{ "-", "<CMD>Oil<CR>", desc = "Open oil" },
-		{ "<C-n>", "<CMD>Oil --float<CR>", desc = "Open oil(float)" },
 	},
 	opts = {
-		float = {
-			max_width = 60,
-			max_height = 25,
-			padding = 5,
-		},
 		view_options = {
-			show_hidden = false,
+			show_hidden = true,
 		},
 	},
-
-	config = function(_, opts)
-		require("oil").setup(opts)
-	end,
 }
