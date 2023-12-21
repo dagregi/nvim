ec3a34e97a7ab4a40926a224a049ded3d5e85b1e
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-21 10:59:58 +0000

    testing out AI code assistance

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,24 @@
+return {
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+	config = function()
+		vim.g.codeium_manual = true
+		local remap = require("config.utils").remap
+		local codeium = { desc = "Codeium: ", expr = true, silent = true }
+		remap("i", "<M-Tab>", function()
+			return vim.fn["codeium#Accept"]()
+		end, "Accept", codeium)
+		remap("i", "<C-p>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, "Completion prev", codeium)
+		remap("i", "<C-n>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, "Completion next", codeium)
+		remap("i", "<C-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, "Clear list", codeium)
+		remap("i", "<C-s>", function()
+			return vim.fn["codeium#Complete"]()
+		end, "Manual completion", codeium)
+	end,
+}
