c3ef0574972a17902bdd31fbb4f0e511553ca0ca
dagregi <120724828+dagregi@users.noreply.github.com>
2024-01-04 18:56:56 +0000

    migrated to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,23 +1,17 @@
 return {
 	"ThePrimeagen/harpoon",
-	keys = {
-		{
-			"<C-g>",
-			function()
-				require("harpoon.ui").toggle_quick_menu()
-			end,
-			desc = "Harpoon UI",
-		},
-	},
+	event = "VeryLazy",
+	branch = "harpoon2",
 	config = function()
-		local ui = require("harpoon.ui")
-		local mark = require("harpoon.mark")
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 		local remap = require("config.utils").remap
-		remap("n", "<leader>ha", function() mark.add_file() end, "Add file")
-		remap("n", "<leader>hc", function() mark.clear_all() end, "Clear marks")
-		remap("n", "<C-i>", function() ui.nav_file(1) end)
-		remap("n", "<C-t>", function() ui.nav_file(2) end)
-		remap("n", "<C-n>", function() ui.nav_file(3) end)
-		remap("n", "<C-s>", function() ui.nav_file(4) end)
+		remap("n", "<leader>ha", function() harpoon:list():append() end, "Add file")
+		remap("n", "<C-h>", function() harpoon:list():select(1) end)
+		remap("n", "<C-t>", function() harpoon:list():select(2) end)
+		remap("n", "<C-n>", function() harpoon:list():select(3) end)
+		remap("n", "<C-s>", function() harpoon:list():select(4) end)
 	end,
 }
