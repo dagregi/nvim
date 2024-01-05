28118fede8ca561a51c7523a2c4dfe2966c6437d
dagregi <120724828+dagregi@users.noreply.github.com>
2024-01-05 08:19:20 +0000

    colorscheme highlight group changes

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,17 +1,41 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	event = "UIEnter",
-	opts = {
-		variant = "main",
-		bold_vert_split = false,
-		disable_background = true,
-		disable_float_background = true,
-		dim_nc_background = true,
-		disable_italics = true,
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		branch = "canary",
+		event = "VeryLazy",
+		priority = 1000,
+		opts = {
+			variant = "main",
+			bold_ver_split = true,
+			dim_inactive_windows = true,
+			extend_background_behind_borders = true,
+			styles = {
+				bold = true,
+				italic = false,
+				transparency = true,
+			},
+			highlight_groups = {
+				TelescopeBorder = { fg = "highlight_high", bg = "none" },
+				TelescopeNormal = { bg = "none" },
+				TelescopePromptNormal = { bg = "base" },
+				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+				TelescopeSelection = { fg = "text", bg = "none" },
+				TelescopeSelectionCaret = { fg = "rose", bg = "none" },
+				String = { fg = "#a6e3a1" },
+				Visual = { fg = "inherit", bg = "highlight_high" },
+				["@field"] = { fg = "#b4befe" },
+				["@property"] = { fg = "#b4befe" },
+				ModesNormal = { fg = "base", bg = "rose", bold = true },
+				ModesCommand = { fg = "base", bg = "love", bold = true },
+				ModesVisual = { fg = "base", bg = "iris", bold = true },
+				ModesReplace = { fg = "base", bg = "#cba6f7", bold = true },
+				ModesInsert = { fg = "base", bg = "foam", bold = true },
+			},
+		},
+		config = function(_, opts)
+			require("rose-pine").setup(opts)
+			vim.cmd.colorscheme("rose-pine")
+		end,
 	},
-	config = function(_, opts)
-		require("rose-pine").setup(opts)
-		vim.cmd.colorscheme("rose-pine")
-	end,
 }
