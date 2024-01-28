f0719155afcc0196f190ef3b250aefa788a32bcb
dagregi <120724828+dagregi@users.noreply.github.com>
2024-01-28 09:59:33 +0000

    tmux navigator

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -19,6 +19,23 @@ return {
 		opts = {},
 	},
 	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{
 		"ahmedkhalf/project.nvim",
 		config = function()
 			require("project_nvim").setup({
